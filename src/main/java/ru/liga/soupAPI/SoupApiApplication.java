becf05c752dbbde41f5422765f1cecd469035be4
akassina <akassina@phoenixit.ru>
2022-07-21 15:37:13 +0300

    Initial commit

---
+++ src/main/java/ru/liga/soupAPI/SoupApiApplication.java
@@ -0,0 +1,13 @@
+package ru.liga.soupAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoupApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoupApiApplication.class, args);
+    }
+
+}
